c0dcec0755ae24e502a0584f906a7e8e3e113e83
cloud <62519659+respecting@users.noreply.github.com>
2021-08-26 18:20:36 -0600

    push

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { webpackModules } from "@cumcord/modules";
+import { patcher } from "@cumcord";
+
+// yeah
+const getStickerAssetUrl = webpackModules.findByProps("getStickerAssetUrl"),
+    getStickerSendability = webpackModules.findByProps("getStickerSendability"),
+    { ComponentDispatch } = webpackModules.findByProps("ComponentDispatch"),
+    { closeExpressionPicker } =  webpackModules.findByProps("closeExpressionPicker");
+
+let pSendability,
+    injectedCSS;
+
+export default {
+    onLoad() {
+        // patch pSendability to send sticker url and inject CSS to remove grayscale
+        pSendability = patcher.before("getStickerSendability", getStickerSendability, ([args]) => {
+            if (args.format_type == 1 || args.format_type == 2) {
+                closeExpressionPicker();
+                return ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
+                    content: " " + getStickerAssetUrl(args)
+                });
+            }
+        });
+        injectedCSS = patcher.injectCSS(`.stickerUnsendable-2q_h2B {
+            webkit-filter: grayscale(0%) !important;
+            filter: grayscale(0%) !important;
+        }`);
+    },
+    onUnload() {
+        // unpatch
+        pSendability();
+        injectedCSS();
+    }
+};
