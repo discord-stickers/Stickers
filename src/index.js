e52044adf7972dea2c251230343d3738c1b02a2e
cloud <62519659+respecting@users.noreply.github.com>
2021-08-30 20:58:26 -0600

    Searching! (paid stickers do not work)

--- src/index.js
+++ src/index.js
@@ -1,40 +1,53 @@
 import { findByProps } from "@cumcord/modules/webpackModules";
 import { showToast } from "@cumcord/ui/toasts";
-import { patcher } from "@cumcord";
+import { before, after, injectCSS } from "@cumcord/patcher";
 
 // yeah
 const getStickerSendability = findByProps("getStickerSendability"),
+    isSendableSticker = findByProps("isSendableSticker"),
     { getStickerAssetUrl } = findByProps("getStickerAssetUrl"),
     { ComponentDispatch } = findByProps("ComponentDispatch"),
     { closeExpressionPicker } = findByProps("closeExpressionPicker"),
     { getCurrentUser } = findByProps("getCurrentUser"),
+    { input, disabled } = findByProps("disabled", "tagLabel"),
     { stickerUnsendable } = findByProps("stickerUnsendable"),
     { stickerAsset } = findByProps("stickerAsset"),
     unpatch = [];
 
 export default {
     onLoad() {
-        if (getCurrentUser().premiumType == 2) return showToast({"title": "Users with Nitro cannot use FreeStickers.", "duration": 5000})
+        if (getCurrentUser().premiumType == 2) return showToast({
+            "title": "Users with Nitro cannot use FreeStickers.",
+            "duration": 5000
+        });
+
+        // inject CSS to remove grayscale
+        unpatch.push(injectCSS(`.${stickerUnsendable} {
+            webkit-filter: grayscale(0%) !important;
+            filter: grayscale(0%) !important;
+        }`));
 
         // patch getStickerSendability to send sticker url
-        unpatch.push(patcher.before("getStickerSendability", getStickerSendability, ([args]) => {
-            if (document.querySelector(`.${stickerAsset}:hover`)) { // check if hovering over sticker to prevent bugs
-                if (args.format_type != 3) {
-                    closeExpressionPicker();
-                    return ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
-                        content: " " + getStickerAssetUrl(args).replace(/=[0-9]{3}/g, "=160")
-                    });
-                }
-            }
+        unpatch.push(before("getStickerSendability", getStickerSendability, ([args]) => {
+            if (!document.querySelector(`.${stickerAsset}:hover`)) return; // check if hovering over sticker to prevent bugs
+            if (args.format_type == 3) return closeExpressionPicker();
+            closeExpressionPicker();
+            return ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
+                content: ` ${getStickerAssetUrl(args).replace(/=[0-9]{3}/g, "=160")}`
+            });
         }));
-        
-        // inject CSS to remove grayscale
-        unpatch.push(patcher.injectCSS(`.${stickerUnsendable}{webkit-filter: grayscale(0%) !important;filter: grayscale(0%) !important;}`));
+
+        unpatch.push(after("isSendableSticker", isSendableSticker, () => {
+            if (!document.querySelector(`.${input}`) && !document.querySelector(`.${disabled}`)) return;
+            document.querySelector(`.${input}`)?.removeAttribute("disabled");
+            document.querySelector(`.${input}`).placeholder = "Search for stickers";
+            document.querySelector(`.${disabled}`)?.classList.remove(disabled);
+        }));
     },
     onUnload() {
         // unpatch
         unpatch.forEach(patch => {
-            patch()
-        })
+            patch();
+        });
     }
 };
