a2ae61828132031fa9b4848d36905ea87564a930
cloud <62519659+lem6ns@users.noreply.github.com>
2021-09-25 12:19:24 -0600

    patch react component instead and start working on ws support

--- src/index.js
+++ src/index.js
@@ -1,56 +1,80 @@
-import { findByProps } from "@cumcord/modules/webpack";
+import { findByProps, find } from "@cumcord/modules/webpack";
 import { showToast } from "@cumcord/ui/toasts";
-import { before, after, injectCSS } from "@cumcord/patcher";
+import { before, instead, injectCSS } from "@cumcord/patcher";
 
 // yeah
-const getStickerSendability = findByProps("getStickerSendability"),
-    isSendableSticker = findByProps("isSendableSticker"),
-    { getStickerAssetUrl } = findByProps("getStickerAssetUrl"),
-    { ComponentDispatch } = findByProps("ComponentDispatch"),
-    { closeExpressionPicker } = findByProps("closeExpressionPicker"),
-    { getCurrentUser } = findByProps("getCurrentUser"),
+const { getCurrentUser } = findByProps("getCurrentUser"),
     { input, disabled } = findByProps("disabled", "tagLabel"),
     { stickerUnsendable } = findByProps("stickerUnsendable"),
-    { stickerAsset } = findByProps("stickerAsset"),
+    { ComponentDispatch } = findByProps("ComponentDispatch"),
+    { closeExpressionPicker } = findByProps("closeExpressionPicker"),
+    sticker = find((m) => m?.default?.displayName === "Sticker"),
     unpatch = [];
 
-export default {
-    onLoad() {
-        if (getCurrentUser().premiumType == 2) return showToast({
-            "title": "Users with Nitro cannot use FreeStickers.",
-            "duration": 5000
-        });
-
-        // inject CSS to remove grayscale
-        unpatch.push(injectCSS(`.${stickerUnsendable} {
-            webkit-filter: grayscale(0%) !important;
-            filter: grayscale(0%) !important;
-        }`));
-
-        // patch getStickerSendability to send sticker url
-        unpatch.push(before("getStickerSendability", getStickerSendability, ([args]) => {
-            // first 2 checks are to fix bugs and 3rd check is to prevent official discord stickers from not working 'cause copyright
-            if (document.activeElement.className == input) return document.querySelector(`.${input}`).blur();
-            if (!document.querySelector(`.${stickerAsset}:hover`)) return;
-            if (args.format_type == 3 || args?.sort_value) return closeExpressionPicker();
-            closeExpressionPicker();
-            return ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
-                content: ` ${getStickerAssetUrl(args).replace(/=[0-9]{3}/g, "=160")}`
+export default (data) => {
+    return {
+        onLoad() {
+            if (getCurrentUser().premiumType == 2) return showToast({
+                "title": "Users with Nitro cannot use FreeStickers.",
+                "duration": 5000
             });
-        }));
-        
-        // patch isSendableSticker to make search work
-        unpatch.push(after("isSendableSticker", isSendableSticker, () => {
-            if (!document.querySelector(`.${input}`) && !document.querySelector(`.${disabled}`)) return;
-            document.querySelector(`.${input}`)?.removeAttribute("disabled");
-            document.querySelector(`.${input}`).placeholder = "Search for stickers";
-            document.querySelector(`.${disabled}`)?.classList.remove(disabled);
-        }));
-    },
-    onUnload() {
-        // unpatch
-        unpatch.forEach(patch => {
-            patch();
-        });
+
+            // if (window["DiscordNative"]) {
+            //     unpatch.push(instead("handleConnection", findByProps("handleConnection").__proto__, (args, orig) => {
+            //         let ws = args[0]
+            //         if ((ws.upgradeReq()).url == "/discord-stickers") {
+            //             ws.on("message", (msg) => {
+            //                 const info = JSON.parse(msg)
+            //                 switch (info.message) {
+            //                     case "INSTALL_PACK":
+            //                         const packName = Object.keys(info.pack)[0],
+            //                             stickers = info.pack[packName].stickers;
+            //                         data.persist.store.packs[packName] = stickers;
+
+            //                         console.log(packName, stickers, data.persist.store.packs)
+            //                         ws.send(JSON.stringify({
+            //                             "message": "SUCCESS"
+            //                         }))
+            //                         ws.close();
+            //                         break;
+            //                 }
+            //             });
+            //         } else {
+            //             return orig(...args);
+            //         }
+            //     }))
+            // }
+
+            // inject CSS to remove grayscale
+            unpatch.push(injectCSS(`.${stickerUnsendable} {
+                webkit-filter: grayscale(0%) !important;
+                filter: grayscale(0%) !important;
+            }`));
+
+            // patch Sticker component to send sticker url & fix search (in a hacky way)
+            unpatch.push(before("default", sticker, ([args]) => {
+                if (args?.sticker?.id && document.querySelector(`img[src*='${args?.sticker?.id}']`)) {
+                    let img = document.querySelector(`img[src*='${args?.sticker?.id}']`);
+
+                    document.querySelector(`.${input}`)?.removeAttribute("disabled");
+                    document.querySelector(`.${input}`).placeholder = "Search for stickers";
+                    document.querySelector(`.${disabled}`)?.classList.remove(disabled);
+                    
+
+                    img.onclick = () => {
+                        closeExpressionPicker();
+                        return ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
+                            content: ` ${img.src.replace(/=[0-9]{2}/g, "=160")}`
+                        });
+                    }
+                }
+            }));
+        },
+        onUnload() {
+            // unpatch
+            unpatch.forEach(patch => {
+                patch();
+            });
+        }
     }
 };
